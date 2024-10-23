7d509ea8cc5ce370ba2438e343bf2b530140ab17
SaffiSardar <saffisardar2012@gmail.com>
2024-10-23 15:11:27 +0500

    env

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css';
 import * as THREE from 'three';
 
@@ -6,30 +8,66 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 //camera
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
+//HDRI
+new RGBELoader()
+    .load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/pond_bridge_night_1k.hdr', function(texture) {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.background = texture;
+        scene.environment = texture;
+    });
+
 //objects
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color:"red"});
-const mesh = new THREE.Mesh(geometry,material);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({
+    color: "black",
+    metalness: 0.7,
+    roughness: 0.2
+});
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+//GLTF model
+const loader = new GLTFLoader();
+loader.load(
+    './DamagedHelmet.gltf',
+    function (gltf) {
+        scene.add(gltf.scene);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.error('An error happened', error);
+    }
+);
 
 //renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#canvas"),
-    antialias:true,
+    antialias: true,
 });
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+//OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 //animate
-
-function animate(){
+function animate() {
     window.requestAnimationFrame(animate);
     
+    //update controls
+    controls.update();
+    
     //render
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
 
 animate();
